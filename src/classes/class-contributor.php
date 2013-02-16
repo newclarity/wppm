bf2b5aa3e38b2e2756c12fe9cdd6fd26669cb2d3
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-16 18:33:50 -0500

    Added WPPM_Contributor->homepage, code to default it in the __construct() method, and a fixed up to support an ROOT->author_homepage so we can support the semantics of this format http://1.shadowcdn.com/files/external-update-example/info.json from http://w-shadow.com/blog/2010/09/02/automatic-updates-for-any-plugin/.

--- src/classes/class-contributor.php
+++ src/classes/class-contributor.php
@@ -5,7 +5,62 @@ class WPPM_Contributor extends WPPM_Container {
   var $CONTAINED_TYPES = array(
     'links' => 'WPPM_Link',
   );
+  var $type;
   var $name;
   var $email;
+  var $homepage;
   var $links = array();
+  function __construct( $type, $value, $config ) {
+    parent::__construct( $type, $value, $config );
+    if ( is_null( $this->name ) && is_string( $value ) ) {
+      /**
+       * The file contains a string name for author, not multiple subproperties
+       * This means 'author_homepage' is probably also included, ala this:
+       *   http://1.shadowcdn.com/files/external-update-example/info.json
+       * From this:
+       *   http://w-shadow.com/blog/2010/09/02/automatic-updates-for-any-plugin/
+       */
+      $this->name = $value;
+    }
+    $this->type = $type;
+    if ( is_null( $this->homepage ) ) {
+      if ( isset( $this->links['homepage'] ) )
+        /**
+         * Homepage not set, but is set in links.
+         */
+        $this->homepage = $this->links['homepage']->url;
+      else if ( count( $this->links ) ) {
+        /**
+         * Homepage not set, get the first link in the links array.
+         */
+        $this->homepage = reset( $this->links )->url;
+      }
+    } else if ( ! preg_match( '#^https?://#', $this->homepage ) ) {
+      /**
+       * Not a URL but a key into the links array.
+       */
+      $this->homepage = $this->links[$this->homepage]->url;
+    }
+  }
+  function _fixup() {
+    if ( 'author' == $this->id ) {
+
+      if ( is_null( $this->homepage ) && isset( $this->ROOT->unused['author_homepage'] ) ) {
+        $this->homepage = $this->ROOT->unused['author_homepage'];
+        unset( $this->ROOT->unused['author_homepage'] );
+      }
+
+      $found = false;
+      foreach( $this->links as $link_id => $link ) {
+        if ( $link->url == $this->homepage ) {
+          $found = true;
+          break;
+        }
+      }
+      if ( ! $found )
+        $this->links['homepage'] = $this->homepage;
+
+      return;
+    }
+  }
 }
