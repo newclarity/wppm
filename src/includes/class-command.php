ee46eda04e0c0120c5fffa23ce51d5a9d99f6fbb
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-17 04:33:53 -0500

    Added errors and messages to WPPM_Command base class.

--- src/includes/class-command.php
+++ src/includes/class-command.php
@@ -2,6 +2,8 @@
 
 abstract class WPPM_Command {
   var $command;
+  var $errors = array();
+  var $messages = array();
   function __construct() {
   }
   function get_command_name() {
@@ -12,7 +14,15 @@ abstract class WPPM_Command {
     $message = sprintf( 'The %s class has not implemented an execute() method.', get_class( $this ) );
     WP_Packager_Manager::fail( $message );
   }
-  function show( $message ) {
+  function add_error( $error ) {
+    $this->errors[] = "ERROR: {$error}";
+  }
+  function add_message( $message ) {
+    $this->messages[] = $message;
+  }
+  function show( $message = false ) {
+    if ( ! $message )
+      $message = implode( "\n\t", $this->messages );
     fwrite( STDOUT, "{$message}\n" );
   }
 }
