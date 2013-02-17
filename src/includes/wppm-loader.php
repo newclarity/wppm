4a28228ebd720e268634ba39d8d932a4152100a2
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-17 04:33:28 -0500

    Included the VCS base files and switched to a named autoloader using spl_autoload_register();

--- src/includes/wppm-loader.php
+++ src/includes/wppm-loader.php
@@ -6,6 +6,11 @@
 require ( __DIR__ . "/wp-package-manager.php" );
 require ( __DIR__ . "/class-container.php" );
 require ( __DIR__ . "/class-command.php" );
+require ( __DIR__ . "/vcs-interface.php" );
+require ( __DIR__ . "/class-vcs-agent.php" );
+
+spl_autoload_register( 'wppm_class_autoloader' );
+
 /**
  * Autoloads classes from the /classes subdirectory.
  *
@@ -15,7 +20,7 @@ require ( __DIR__ . "/class-command.php" );
  *
  * @throws Exception
  */
-function __autoload( $class_name ) {
+function wppm_class_autoloader( $class_name ) {
   $class_file = strtolower( str_replace( '_', '-', preg_replace( '#^WPPM_(.*)$#', '$1', $class_name ) ) );
   $class_file = dirname( __DIR__ ) . "/classes/class-{$class_file}.php";
   if ( file_exists( $class_file ) ) {
