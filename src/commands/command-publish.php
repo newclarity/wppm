0e0dab191570e66de27d798b144b53060ad6e95c
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-18 09:32:59 -0500

    Implemented 'publish' command but have not tested it.  Need to set up test SVN account somehow to test before pushing to WordPress.org.

--- src/commands/command-publish.php
+++ src/commands/command-publish.php
@@ -8,5 +8,14 @@ class WPPM_Publish_Command extends WPPM_Command {
   function execute( $wppm ) {
     $package = $wppm->get_package();
     $config = $wppm->get_config();
+    $work_dir = $wppm->get_wppm_filepath( "work/{$package->slug}/{$package->version}" );
+    $prepared_dir = "{$work_dir}/prepared";
+
+    $svn_agent = $wppm->get_vcs_agent( 'svn', 'wordpress.org' );
+
+    echo "\nPublishing {$package->type} {$package->name} {$package->version} to WordPress.org...\n";
+    $this->show( $svn_agent->push_version( $prepared_dir, $package->version ) );
+
+
   }
 }
