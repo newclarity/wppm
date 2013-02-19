b0a99082258d9999fd31dc7552ae3fd36b14974c
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-19 04:13:52 -0500

    Changed to array_merge($a,b) instead of failing $a+b and added comment headers and a bit better variable names.

--- src/classes/class-file-ops.php
+++ src/classes/class-file-ops.php
@@ -2,29 +2,70 @@
 class File_Ops {
   static $_dir_stack = array();
 
-  static function ensure_dir( $dir ) {
+  /**
+   * @param string $directory
+   *
+   * @return array
+   */
+  static function ensure_dir( $directory ) {
     $output = array();
-    if ( ! empty( $dir ) ) {
-      if( ! is_dir( $dir ) ) {
+    if ( ! empty( $directory ) ) {
+      if( ! is_dir( $directory ) ) {
         $output = array();
-        @mkdir( $dir );
-        if ( ! is_dir( $dir ) ) {
-          $output += self::ensure_dir( dirname( $dir ) );
-          mkdir( $dir );
+        @mkdir( $directory );
+        if ( ! is_dir( $directory ) ) {
+          $output = array_merge( $output, self::ensure_dir( dirname( $directory ) ) );
+          mkdir( $directory );
         }
-        $output[] = "Making directory {$dir}...";
+        $output[] = "Making directory {$directory}...";
       }
     }
   	return $output;
   }
 
-  static function mirror_dir( $from_dir, $to_dir ) {
-    $from_dir = rtrim( $from_dir, '/' );
-    $to_dir = rtrim( $to_dir, '/' );
-    exec( "rsync -vaz {$from_dir}/ {$to_dir}", $output );
+  /**
+   * @param string $source_dir
+   * @param string $destination_dir
+   *
+   * @return array
+   */
+  static function mirror_dir( $source_dir, $destination_dir ) {
+    $source_dir = rtrim( $source_dir, '/' );
+    $destination_dir = rtrim( $destination_dir, '/' );
+    exec( "rsync -vaz {$source_dir}/ {$destination_dir}", $output );
     return $output;
   }
 
+  /**
+   * @param string $source_dir
+   * @param string $destination_dir
+   *
+   * @return array
+   */
+  static function mirror_dir_php( $source_dir, $destination_dir ) {
+    $output = array();
+  	$dir = opendir( $source_dir );
+  	self::ensure_dir( $destination_dir );
+  	while( false !== ( $file = readdir( $dir ) ) ) {
+  		if ( '.' == $file || '..' == $file )
+  			continue;
+  		if ( is_dir( "{$source_dir}/{$file}" ) ) {
+        $output = array_merge( $output,  self::mirror_dir_php( "{$source_dir}/{$file}" , "{$destination_dir}/{$file}" ) );
+  		} else {
+  			copy( $from = "{$source_dir}/{$file}" , $to = "{$destination_dir}/{$file}" );
+        $output[] = "Copying {$from} to {$to}...";
+  		}
+  	}
+  	closedir( $dir );
+  	return $output;
+  }
+
+  /**
+   * @param string $dir_to_zip
+   * @param string $zip_file
+   *
+   * @return array
+   */
   static function zip_dir( $dir_to_zip, $zip_file ) {
     $dir_to_zip = rtrim( $dir_to_zip, '/' );
     $parent_dir = dirname( $dir_to_zip );
@@ -36,52 +77,58 @@ class File_Ops {
     return $output;
   }
 
+  /**
+   * @param string $from_dir
+   * @param string $to_dir
+   *
+   * @return array
+   */
   static function move_dir( $from_dir, $to_dir ) {
     $from_dir = rtrim( $from_dir, '/' );
     $to_dir = rtrim( $to_dir, '/' );
     $output1 = self::ensure_dir( $to_dir );
     exec( "mv {$from_dir}/*.* {$to_dir}/", $output2 );
-    return $output1 + $output2;
+    return array_merge( $output1, $output2 );
   }
 
+  /**
+   * @param string $filepath
+   * @param string $new_dir
+   *
+   * @return array
+   */
   static function move_file( $filepath, $new_dir ) {
     $new_dir = rtrim( $new_dir, '/' );
     $output1 = self::ensure_dir( $new_dir );
     exec( "mv {$filepath} {$new_dir}/", $output2 );
-    return $output1 + $output2;
+    return array_merge( $output1, $output2 );
   }
 
-  static function mirror_dir_php( $source, $destination ) {
-    $output = array();
-  	$dir = opendir( $source );
-  	self::ensure_dir( $destination );
-  	while( false !== ( $file = readdir( $dir ) ) ) {
-  		if ( '.' == $file || '..' == $file )
-  			continue;
-  		if ( is_dir( "{$source}/{$file}" ) ) {
-        $output += self::mirror_dir_php( "{$source}/{$file}" , "{$destination}/{$file}" );
-  		} else {
-  			copy( $from = "{$source}/{$file}" , $to = "{$destination}/{$file}" );
-        $output[] = "Copying {$from} to {$to}...";
-  		}
-  	}
-  	closedir( $dir );
-  	return $output;
-  }
-
+  /**
+   * @param string $dir
+   *
+   * @return array
+   */
   static function remake_dir( $dir ) {
-    $output = array();
-  	if ( is_dir( $dir ) )
-      $output += self::kill_dir( $dir );
-    $output += self::ensure_dir( $dir );
-  	return $output;
+    $output = is_dir( $dir ) ? self::kill_dir( $dir ) : array();
+    return array_merge( $output,  self::ensure_dir( $dir ) );
   }
 
+  /**
+   * @param string $dir
+   *
+   * @return array
+   */
   static function kill_dir( $dir ) {
     exec( "rm -r -f {$dir}", $output );
     return $output;
   }
 
+  /**
+   * @param string $file
+   *
+   * @return array
+   */
   static function kill_file( $file ) {
     exec( "rm -r -f {$file}", $output );
     return $output;
@@ -92,7 +139,8 @@ class File_Ops {
    *
    * @see: http://php.net/manual/en/function.rmdir.php
    *
-   * @param $directory
+   * @param string $directory
+   * @return array
    */
   static function remove_directory( $directory ) {
     $output = array();
@@ -101,7 +149,7 @@ class File_Ops {
         if ( '.' == $entry || '..' == $entry ) {
       		continue;
         } else if ( is_dir( $entry_dir = "{$directory}/{$entry}" ) === true ) {
-          $output += self::remove_directory( $entry_dir );
+          $output = array_merge( $output,  self::remove_directory( $entry_dir ) );
         } else {
           $output[] = "Removing {$entry_dir}...";
           unlink( $entry_dir );
@@ -122,6 +170,8 @@ class File_Ops {
    *
    * @param string $directory Directory in which files or subdirs to delete may be found.
    * @param string $file_or_subdir Delete this file or directory recursively (turtles all the way down)
+   * @return array
+   *
    */
   static function recursive_remove( $directory, $file_or_subdir ) {
     $output = array();
@@ -130,20 +180,27 @@ class File_Ops {
         if ( '.' == $entry || '..' == $entry ) {
       		continue;
         } else if ( $file_or_subdir == $entry ) {
-          $output += self::kill_dir( "{$directory}/{$file_or_subdir}" );
+          $output = array_merge( $output,  self::kill_dir( "{$directory}/{$file_or_subdir}" ) );
         } else if ( is_dir( $entry_dir = "{$directory}/{$entry}" ) === true ) {
-          $output += self::recursive_remove( $entry_dir, $file_or_subdir );
+          $output = array_merge( $output,  self::recursive_remove( $entry_dir, $file_or_subdir ) );
   			}
   		}
   		closedir( $handle );
   	}
   	return $output;
   }
+
+  /**
+   * @param string $new_dir
+   */
   private static function _pushdir( $new_dir ) {
     array_push( self::$_dir_stack, getcwd() );
     chdir( $new_dir );
   }
 
+  /**
+   *
+   */
   private static function _popdir() {
     chdir( array_pop( self::$_dir_stack ) );
   }
