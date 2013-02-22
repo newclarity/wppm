dc3bbfc8705bafdd6ce94535c59272dc5b92f636
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-22 06:03:29 -0500

    Restructured WPPM_License to require a license file be found in the root. Saved the GPLv2 text in a get_text() function for future use.

--- src/classes/class-license.php
+++ src/classes/class-license.php
@@ -4,10 +4,29 @@ class WPPM_License extends WPPM_Container {
   var $type;
   var $url;
   var $text;
-  function __construct( $domain, $value, $config ) {
-    parent::__construct( $domain, null, $config );
+  function __construct( $id, $value, $config ) {
+    parent::__construct( $id, null, $config );
     $gpl2_url = "http://www.gnu.org/licenses/gpl-2.0.html";
-    $gpl2_text = <<<TEXT
+    if ( is_null( $this->type ) ) {
+  	  $this->type = "GPL v2 or later";
+      if ( is_null( $this->url ) )
+  	    $this->url = "http://www.gnu.org/licenses/gpl-2.0.html";
+    }
+    if ( preg_match( '#GPL.*2#', $this->type ) ) {
+      if ( is_null( $this->url ) )
+        $this->url = $gpl2_url;
+    }
+    /**
+     * Existence of a license.txt file trumps all.
+     */
+    if ( is_file( $license_file = getcwd() . '/license.txt' ) ) {
+      $license_text = file_get_contents( $license_file );
+      if ( ! empty( $license_text ) )
+        $this->text = $license_text;
+    }
+  }
+  function get_text() {
+    return <<<TEXT
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
 published by the Free Software Foundation.
@@ -21,18 +40,5 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 TEXT;
-    if ( is_null( $this->type ) ) {
-  	  $this->type = "GPL v2 or later";
-      if ( is_null( $this->url ) )
-  	    $this->url = "http://www.gnu.org/licenses/gpl-2.0.html";
-      if ( is_null( $this->text ) )
-      	$this->text = $gpl2_text;
-    }
-    if ( preg_match( '#GPL.*2#', $this->type ) ) {
-      if ( is_null( $this->url ) )
-        $this->url = $gpl2_url;
-      if ( is_null( $this->text ) )
-  	    $this->text = $gpl2_text;
-    }
   }
 }
